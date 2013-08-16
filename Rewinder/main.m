661179f629ca4b0839f7088bdb5db5aa257b6888
Pedro <Pedro@MacBook-de-User.local>
2013-08-15 23:30:06 -0300

    Initial Commit

---
+++ Rewinder/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Rewinder
+//
+//  Created by Pedro on 15/08/13.
+//  Copyright Anonymous 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
